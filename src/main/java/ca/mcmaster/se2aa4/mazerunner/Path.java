d589ec0acb277de328e8b2e7b35432cd41d36f0e
yug-vashisth <yugdv27@gmail.com>
2025-02-02 21:12:17 -0500

    added comments to last uncommented class

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -2,18 +2,31 @@ package ca.mcmaster.se2aa4.mazerunner;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Name: Yug Vashisth, MacID: vashisty, Student number: 400501750
+ * The Path class is responsible for getting the canonical and factorized form of the path.
+ * it calculates the factorized path, and displays the canoincal straight up once converting tostring
+ */
 public class Path {
     private final List<Character> steps;
 
+    /**
+     * Constructs an empty Path object.
+     */
     public Path() {
         this.steps = new ArrayList<>();
     }
 
+    /**
+     * adds step
+     */
     public void addStep(char step) {
         steps.add(step);
     }
 
+    /**
+     * Returns canonical form of the path.
+     */
     public String getCanonicalForm() {
         StringBuilder canonicalForm = new StringBuilder();
         for (char step : steps) {
@@ -21,6 +34,10 @@ public class Path {
         }
         return canonicalForm.toString();
     }
+
+    /**
+     * Returns factorized form of the path.
+     */
 
     public String getFactorized() {
         if (steps.isEmpty()) {
@@ -42,6 +59,6 @@ public class Path {
         }
         factorized.append(count).append(currentStep);
 
-        return factorized.toString();
+        return factorized.toString(); //tostring method to return it in proper output format
     }
 }
